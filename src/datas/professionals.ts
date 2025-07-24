0c1abc20accced475142a761ce656c11983aab75
MARTIN Thomas <thomas3.martin@epitech.eu>
2025-07-24 23:17:36 +0200

    add experiences

--- src/datas/professionals.ts
+++ src/datas/professionals.ts
@@ -15,19 +15,29 @@ export const professionalsData: ProfessionalItem[] = [
     date: "2023 - 2024",
     title: "Développeur Full Stack",
     company: "Carrevolutis",
-    description: "J’ai participé au développement full stack et à la maintenance des plateformes de Carrevolutis dans un cadre Agile. Mes missions comprenaient la création de nouvelles fonctionnalités, le refactoring de code existant et la résolution de bugs en collaboration avec les équipes techniques.",
-    technologies: ["React.js", "PHP", "Symphony", "TypeScript", "Git"],
+    description: "Participation à la maintenance et au développement des plateformes CARREVOLUTIS (Dogfinance.com, Tekkit.io, Bizzeo.co, admin, client). Contribution à la création de nouvelles fonctionnalités front-end et back-end dans un environnement Agile et au refactoring du code. Collaboration avec l’équipe pour résoudre les bugs rencontrés sur les plateformes.",
+    technologies: ["React.js", "PHP", "Symphony", "TypeScript", "Scrum", "Git"],
     image: "/images/logo.png",
     location: "Paris, France"
   },
   {
     id: 2,
-    date: "2023 - 2024",
-    title: "Développeur Full Stack",
-    company: "Eventify",
-    description: "",
-    technologies: ["React.js", "PHP", "Symphony", "TypeScript", "Git"],
-    image: "/images/eventify.png",
+    date: "2022 - 2024",
+    title: "Développeur Mobile",
+    company: "Epitech",
+    description: "Développement d'une application mobile Flutter pour l'accompagnement psychologique, intégrant l'API OpenAI pour fournir des réponses personnalisées aux utilisateurs. Conception de l'interface et gestion de l'état de l'application, ainsi que participation à la création de nouvelles fonctionnalités côté front-end et back-end dans un environnement Agile (Scrum).",
+    technologies: ["Flutter", "Node.js", "MongoDB", "Scrum", "Git"],
+    image: "/images/epitech.png",
+    location: "Paris, France"
+  },
+  {
+    id: 3,
+    date: "2021",
+    title: "Stage développeur Web",
+    company: "SIDR",
+    description: "Création d’un parcours conversationnel via Google Dialogflow, intégré à une application web Flutter. Mise en place d’une boîte de dialogue interactive avec des questions/réponses guidées par boutons et redirection dynamique selon les choix de l'utilisateur.",
+    technologies: ["Flutter", "Google DialogFlow", "Git"],
+    image: "/images/logo-sidr.png",
     location: "Paris, France"
   }
 ]
